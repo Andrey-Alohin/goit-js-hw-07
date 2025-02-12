bb289e67dd2748d51c45114f99effb005e3d6387
Andrey-Alohin <andriealohin2020@outlook.com>
2025-02-13 00:48:05 +0200

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,34 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", submit)
 
+function submit (event) {
+    event.preventDefault();
+    const formData = event.target;
+    const userData = {};
+    userData.email = formData.elements.email.value.trim();
+    userData.password = formData.elements.password.value.trim();
+    if (userData.email.replace(/ /g, "") !== "" && userData.password.replace(/ /g, "") !== "") {
+        console.log(userData);
+        formData.reset();
+    } else {
+        alert("All form must be filled in");
+    }
+}
+
+
+
+// function submit (event) {
+//     event.preventDefault();
+//     const userData = {};
+//     const formData = new FormData(event.target);
+//     formData.forEach((value, key) => {
+//         if (value.replace(/ /g, "") !== "") {
+//            userData[key] = value.trim(); 
+//         } else  {
+//             value = "";
+//             alert(`All form must be filled in. Don't use spaces in ${key}`);
+//             return;
+//         }
+//     })
+//     console.log(userData);
+// }
