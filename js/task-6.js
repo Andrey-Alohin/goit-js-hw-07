9900df54f17efe18472b11eb5eb121af4833dd85
Andrey-Alohin <andriealohin2020@outlook.com>
2025-02-13 03:04:47 +0200

    task-6 done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const container = document.querySelector("#boxes");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+
+function destroyBoxes() {
+  let boxes = container.querySelectorAll("div");
+  for (const box of boxes) {
+    box.remove();
+  }
+}
+
+function createBoxes() {
+  const amount = input.value;
+  if (1 <= amount && amount <= 100) {
+    destroyBoxes();
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.style.width = `${size}px`;
+    newDiv.style.height = `${size}px`;
+    newDiv.style.backgroundColor = getRandomHexColor();
+    container.append(newDiv);
+    size += 10;
+    } 
+  }
+}
+
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
